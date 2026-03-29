a42f1588936a8c2140f81e322c58793820b63753
Jesse Nwaokolo <jungkookyung61@gmail.com>
2026-03-29 17:23:34 +0100

    First commit

---
+++ app/onboarding.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Onbaording = () => {
+  return (
+    <View>
+      <Text>Onbaording</Text>
+    </View>
+  )
+}
+
+export default Onbaording
